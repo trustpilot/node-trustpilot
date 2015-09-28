06120e4adfbca97ec342653188afe29522c7e247
Luke Pommersheim <lfp@trustpilot.com>
2015-09-28 13:21:59 +0200

    pass things through using the accessProvider class now

--- src/client.js
+++ src/client.js
@@ -1,14 +1,17 @@
 'use strict';
 
 let Review = require('./reviewApi/review');
+let AccessProvider = require('./AccessProvider');
 
 class Client {
-  constructor (apiKey) {
+  constructor (apiKey, secret) {
     this.apiKey = apiKey;
     this.host = 'https://api.tp-staging.com';
-    this.review = new Review(this.apiKey, this.host);
-  }
+    this.secret = secret;
+    this.accessProvider = new AccessProvider(this.apiKey, this.host, this.secret);
+    this.review = new Review(this.accessProvider);
 
+  }
 }
 
 module.exports = Client;
