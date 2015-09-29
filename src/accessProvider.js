67490b1af966437a56e864064970f2441b4750cf
Luke Pommersheim <lfp@trustpilot.com>
2015-09-29 16:24:31 +0200

    generateToken only works now if generateTokenPromise is not set

--- src/accessProvider.js
+++ src/accessProvider.js
@@ -7,22 +7,35 @@ class AccessProvider {
     this.apiKey = apiKey;
     this.host = host;
     this.secret = secret;
+
+    //holds the token promise so we don't generate many
+    let generateTokenPromise;
   }
 
+  //generates a new new oauth token object only if there is no generateTokenPromise set
+  //returns just the access_token
   generateToken () {
-    return request({
-      baseUrl: this.host,
-      uri: '/v1/oauth/system-users/token',
-      method: 'POST',
-      json: true,
-      headers: {
-        Authorization: 'Basic ' + new Buffer(this.apiKey + ':' + this.secret).toString('base64'),
-        'X-Access': new Buffer('{type: "Full"}').toString('base64'),
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: 'grant_type=client_credentials'
-    }).then(function (response) {
-      return response.access_token;
+    if (!this.generateTokenPromise) {
+
+      this.generateTokenPromise = request({
+        baseUrl: this.host,
+        uri: '/v1/oauth/system-users/token',
+        method: 'POST',
+        json: true,
+        headers: {
+          Authorization: 'Basic ' + new Buffer(this.apiKey + ':' + this.secret).toString('base64'),
+          'X-Access': new Buffer('{type: "Full"}').toString('base64'),
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'grant_type=client_credentials'
+      }).then((response) => {
+        this.authorization = response;
+        return response.access_token;
+      });
+    }
+
+    return this.generateTokenPromise;
+  }
     });
   }
 }
