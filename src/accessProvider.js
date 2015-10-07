fb68fae8517e147e4049d2905b5127bcc5533567
Luke Pommersheim <lfp@trustpilot.com>
2015-10-07 15:40:59 +0200

    error handle on token creation

--- src/accessProvider.js
+++ src/accessProvider.js
@@ -23,9 +23,13 @@ class AccessProvider {
         'Content-Type': 'application/x-www-form-urlencoded'
       },
       body: 'grant_type=client_credentials'
-    }).then((response) => {
+    })
+    .then((response) => {
       this.authorization = response;
       return this.authorization;
+    })
+    .catch(function (error) {
+      throw new Error(error);
     });
   }
 
