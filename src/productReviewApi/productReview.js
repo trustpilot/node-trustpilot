ad707073ff16a472a8676e8b0b027da8cf0213ee
Luke Pommersheim <luke@Lukes-MacBook-Air.local>
2015-10-10 15:56:00 +0200

    add some more product review endpoints

--- src/productReviewApi/productReview.js
+++ src/productReviewApi/productReview.js
@@ -77,6 +77,50 @@ class ProductReview {
   getPrivateSingle (reviewId) {
     return this.request.get(`/v1/private/product-reviews/${reviewId}`, true);
   }
+
+  /**
+   * [Prepares the review for participating in a conversation by issuing a conversation ID]
+   * @param {[string]} reviewId [Required. The id of the product review]
+   * @return {[object]} [object containing the conversationId]
+   */
+  createConversation (reviewId) {
+    return this.request.get(`/v1/private/product-reviews/${reviewId}/create-conversation`, true);
+  }
+
+  /**
+   * [Get a summary of product reviews for a single or multiple SKU(s) or productUrl(s). Note: Even though productUrl
+   *  and sku are listed as optional parameters at least one of them must be specified. If both sku and productUrl are
+   *  specified the productUrl is ignored and a search will be made only on the basis of the sku. The summary contains
+   *  information about stars average, distribution and count.]
+   *  @param {[businessUnitId]} string [Required. The id of the business unit. ]
+   *  @param {[object]} options [optional options object]
+   *  @param {[string]} options.productUrl [The url of the product. Mandatory only if no sku is provided, else ignored. ]
+   *  @param {[string]} options.sku [A single or multiple comma-separated SKUs of the product(s). Optional only if productUrl is provided. ]
+   *  @return {[object]} [object containing product reviews summary]
+   */
+  getSummary (businessUnitId, options) {
+    return this.request.get(`/v1/product-reviews/business-units/${businessUnitId}`, false, options);
+  }
+
+  /**
+   * [This method allows you to get business unit product reviews for SKUs and / or productUrls. Note: Even though
+   *  productUrl and sku are listed as optional parameters at least one of them must be specified. It includes review
+   *  content, date of creation of review, individual star rating, id and display name of the consumer who wrote the review.
+   *  Pagination and filtering reviews by language is also possible.]
+   * @param  {[string]} businessUnitId [ Required string.]
+   * @param {[object]} options [optional options object]
+   * @param {[number]} options.page [The page to retrieve. If the page number requested is higher than the available
+   * number of pages an empty array will be returned. Constraints: The allowed range is minimum: 1, maximum: 2147483647 ]
+   * @param {[number]} options.perPage [The number of reviews to retrieve per page. Constraints: The allowed range is minimum: 1, maximum: 100 Default value: 20 ]
+   * @param {[string]} options.productUrl [The url of the product. Optional only if sku is provided.]
+   * @param {[string]} options.sku [The sku of the product. Optional only if productUrl is provided.]
+   * @param {[string]} options.language [Filter reviews by language]
+   * @return {[object]}                [object containing product reviews]
+   */
+  getProductReviews (businessUnitId, options) {
+    return this.request.get(`/v1/product-reviews/business-units/${businessUnitId}/reviews`, false, options);
+  }
+
 }
 
 module.exports = ProductReview;
