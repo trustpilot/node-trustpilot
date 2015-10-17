e6f44900c7067096047be38d356e22af5626fe33
Luke Pommersheim <luke@Lukes-MacBook-Air.local>
2015-10-17 15:10:00 +0200

    init product review spec

---
+++ spec/productReviews.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var ProductReview = require('../src/productReviewApi/productReview');
+var chai = require('chai');
+var should = chai.should();
+var expect = chai.expect;
+var chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+
+describe('ProductReview Api', function () {
+
+});
