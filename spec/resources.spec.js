6b12618b3b8d4abb4b8f5815443dd31ce2f92a2d
Luke Pommersheim <luke@Lukes-MacBook-Air.local>
2015-10-17 14:54:18 +0200

    init resources spec

---
+++ spec/resources.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var Resource = require('../src/resourcesApi/resources');
+var chai = require('chai');
+var should = chai.should();
+var expect = chai.expect;
+var chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+
+describe('Resources Api', function () {
+  
+});
