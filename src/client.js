79d5d2b643dc08b3098ee68321e69c09c71914c6
Miklos Aubert <mat@trustpilot.com>
2017-03-31 14:13:08 +0200

    Remove wrappers and associated unit tests

--- src/client.js
+++ src/client.js
@@ -1,14 +1,7 @@
 'use strict';
 
-const Review = require('./reviewApi/review');
-const Resources = require('./resourcesApi/resources');
 const AccessProvider = require('./accessProvider');
 const Request = require('./requestHelper');
-const ProductReview = require('./productReviewApi/productReview');
-const Invitation = require('./invitationApi/invitation');
-const Consumer = require('./consumerApi/consumer');
-const Categories = require('./categoriesApi/categories');
-const BusinessUnit = require('./businessUnitApi/businessUnit');
 
 class Trustpilot {
   constructor(config) {
@@ -19,13 +12,6 @@ class Trustpilot {
     this.password = config.password;
     this.accessProvider = new AccessProvider(this.apiKey, this.secret, this.username, this.password, this.host);
     this.request = new Request(this.accessProvider);
-    this.review = new Review(this.request);
-    this.resources = new Resources(this.request);
-    this.productReview = new ProductReview(this.request);
-    this.invitation = new Invitation(this.request);
-    this.consumer = new Consumer(this.request);
-    this.categories = new Categories(this.request);
-    this.businessUnit = new BusinessUnit(this.request);
   }
 }
 
