f66b71b6f148904fb427c615f6c4b4b80f1f36d3
Miklos Aubert <mat@trustpilot.com>
2017-03-31 14:24:26 +0200

    Use baseUrl in config to better align with npm request

--- src/client.js
+++ src/client.js
@@ -6,11 +6,11 @@ const Request = require('./requestHelper');
 class Trustpilot {
   constructor(config) {
     this.apiKey = config.apiKey;
-    this.host = config.dev || 'https://api.trustpilot.com';
+    this.baseUrl = config.baseUrl || 'https://api.trustpilot.com';
     this.secret = config.secret || '';
     this.username = config.username;
     this.password = config.password;
-    this.accessProvider = new AccessProvider(this.apiKey, this.secret, this.username, this.password, this.host);
+    this.accessProvider = new AccessProvider(this.apiKey, this.secret, this.username, this.password, this.baseUrl);
     this.request = new Request(this.accessProvider);
   }
 }
