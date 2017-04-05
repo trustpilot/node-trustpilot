08e1dbc1bbb85342441c4efe4d991c358672b213
Miklos Aubert <mat@trustpilot.com>
2017-04-05 11:47:35 +0200

    Just beautifying

--- spec/accessProvider.spec.js
+++ spec/accessProvider.spec.js
@@ -9,86 +9,83 @@ var accessProvider;
 
 chai.use(chaiAsPromised);
 
-describe('Acesss Provider Api ', function () {
-  beforeEach(function (done) {
-
-    accessProvider = new AccessProvider('fakeKey', 'http://fakeHost.com', 'fakeSecret');
-
+describe('Acesss Provider Api ', () => {
+  beforeEach((done) => {
+    accessProvider = new AccessProvider('fakeKey', 'fakeSecret', 'user', 'pass', 'http://fakeHost.com');
     done();
   });
 
-  afterEach(function (done) {
+  afterEach((done) => {
     accessProvider = {};
-
     done();
   });
 
-  describe('the getAccessToken function', function () {
-    describe('when the authorization token exists and is valid', function () {
-      it('should resolve', function () {
+  describe('the getAccessToken function', () => {
+    describe('when the authorization token exists and is valid', () => {
+      it('should resolve', () => {
         accessProvider.authorization = {
-          issued_at: Date.now(), //new Date
-          expires_in: 360000,
-          access_token: 123456789
+          'issued_at': Date.now(),
+          'expires_in': 360000,
+          'access_token': 123456789
         };
 
-        return accessProvider.getAccessToken().then(function (data) {
+        return accessProvider.getAccessToken().then((data) => {
           expect(data).to.equal(accessProvider.authorization.access_token);
         });
       });
     });
 
-    describe('when the authorization token does not exist', function () {
-      it('should resolve with a token because one is made', function () {
-        accessProvider.generateTokenObject = function () {
+    describe('when the authorization token does not exist', () => {
+      it('should resolve with a token because one is made', () => {
+        accessProvider.generateTokenObject = () => {
           return Promise.resolve({
-            access_token: 68465341496
+            'access_token': 68465341496
           });
         };
 
-        return accessProvider.getAccessToken().then(function (data) {
+        return accessProvider.getAccessToken().then((data) => {
           expect(data).to.equal(68465341496);
         });
       });
     });
 
-    describe('when the promise is rejected', function () {
-      it('should handle the promise rejection', function () {
-        accessProvider.generateTokenObject = function () {
+    describe('when the promise is rejected', () => {
+      it('should handle the promise rejection', () => {
+        accessProvider.generateTokenObject = () => {
           return Promise.reject(new Error());
         };
 
-        return accessProvider.getAccessToken().catch(function (data) {
+        return accessProvider.getAccessToken().catch((data) => {
           expect(data).to.deep.equal(Error());
         });
       });
     });
   });
 
-  describe('the isTokenValid function is called', function () {
-    describe('when the accessProvider does not have an authorization object', function () {
-      it('should return false', function () {
+  describe('the isTokenValid function is called', () => {
+    describe('when the accessProvider does not have an authorization object', () => {
+      it('should return false', () => {
         expect(accessProvider.isTokenValid()).to.equal(false);
       });
     });
 
-    describe('when the authorization access token is no longer valid', function () {
-      it('should return false', function () {
+    describe('when the authorization access token is no longer valid', () => {
+      it('should return false', () => {
         accessProvider.authorization = {
-          issued_at: 592786800000, //Fri Oct 14 1988 00:00:00 GMT+0100 (CET)
-          expires_in: 360000
+          'issued_at': 592786800000, // Fri Oct 14 1988 00:00:00 GMT+0100 (CET)
+          'expires_in': 360000
         };
 
         expect(accessProvider.isTokenValid()).to.equal(false);
       });
     });
 
-    describe('when the authorization access token is valid', function () {
-      it('should return true', function () {
+    describe('when the authorization access token is valid', () => {
+      it('should return true', () => {
 
         accessProvider.authorization = {
-          issued_at: Date.now(), //new Date
-          expires_in: 360000
+          'issued_at': Date.now(), //new Date
+          'expires_in': 360000
         };
 
         expect(accessProvider.isTokenValid()).to.equal(true);
