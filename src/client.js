0ba1275cbcf08b09bd822a935541df235b513754
Daniel Lazarovici <lazarov.daniel@gmail.com>
2017-11-02 13:03:22 +0100

    Client can be initialise with access token

--- src/client.js
+++ src/client.js
@@ -4,22 +4,17 @@ const AccessProvider = require('./accessProvider');
 const RequestHelper = require('./requestHelper');
 
 class Trustpilot {
-  constructor(config) {
-    this.apiKey = config.apiKey;
-    this.baseUrl = config.baseUrl || 'https://api.trustpilot.com';
-    this.secret = config.secret || '';
-    this.username = config.username;
-    this.password = config.password;
-    this.tokenRequest = config.tokenRequest;
+  constructor({ apiKey, baseUrl = 'https://api.trustpilot.com', secret = '', username, password, tokenRequest, accessToken }) {
+    const accessProvider = new AccessProvider(apiKey, secret, username, password, baseUrl, tokenRequest, accessToken);
+    this.requestHelper = new RequestHelper(accessProvider);
+  }
 
-    const accessProvider = new AccessProvider(this.apiKey, this.secret, this.username, this.password,
-      this.baseUrl, this.tokenRequest);
-    const requestHelper = new RequestHelper(accessProvider);
+  get apiRequest() {
+    return this.requestHelper.apiRequest;
+  }
 
-    this.apiRequest = requestHelper.apiRequest;
-    this.authenticate = () => {
-      return requestHelper.buildAuthenticatedRequest();
-    };
+  authenticate() {
+    return this.requestHelper.buildAuthenticatedRequest();
   }
 }
 
