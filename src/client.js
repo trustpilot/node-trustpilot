e3815882580384da54b8968d4571569f7d3b5701
Paulo Cunha <pc@paulocunha.me>
2017-11-15 17:03:12 +0100

    mantain the current API for the client

--- src/client.js
+++ src/client.js
@@ -7,6 +7,13 @@ class Trustpilot {
   constructor({ apiKey, baseUrl = 'https://api.trustpilot.com', secret = '', username, password, tokenRequest, accessToken }) {
     const accessProvider = new AccessProvider(apiKey, secret, username, password, baseUrl, tokenRequest, accessToken);
     this.requestHelper = new RequestHelper(accessProvider);
+
+    this.apiKey = apiKey;
+    this.baseUrl = baseUrl;
+    this.secret = secret;
+    this.username = username;
+    this.password = password;
+    this.tokenRequest = tokenRequest;
   }
 
   get apiRequest() {
